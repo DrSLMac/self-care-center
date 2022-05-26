9ee8e656a838dee46320c652776f737d724b6126
Shauna MacFarlane <shauna.macfarlane579@gmail.com>
2022-05-26 00:53:18 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// Identify variables
+//
+//
+//
+// Screen view
+//
+//
+//
+// Buttons/Click
+//
+//
+//
+// form input data?
+//
+//
+//
+// addEventListener('click', function)
+//
+//
+//
+// Event handlers & functions
