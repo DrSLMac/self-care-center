fb3ecf04f3a1798d06aba0e6e89f54e41d5dcf5e
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-05-30 14:07:29 -0500

    add functionality to button

--- src/main.js
+++ src/main.js
@@ -3,76 +3,82 @@ var affirmationRadio = document.getElementById('radio1');
 var mantraRadio = document.getElementById('radio2');
 var receiveButton = document.querySelector(".receiveMessageButton")
 var receivedMessage = document.querySelector("receive-message")
+var selectedMessage = document.querySelector(".selectedMessage")
+var lotusPose = document.querySelector(".lotusPose")
+var clickToClear = document.querySelector(".clickToClear");
 var currentMessage ;
 
 // 👇🏼 addEventListener('click', function)
-receiveButton.addEventListener('click', receiveMessage)
+window.addEventListener("load", homeView)
+receiveButton.addEventListener("click", pushButton)
+clickToClear.addEventListener("click", clickToClearButton)
 
 
 // 👇🏼 Get a random mantra
+function homeView() {
+  lotusPose.classList.remove("hidden");
+  selectedMessage.classList.add("hidden");
+  clickToClear.classList.add("hidden");
+}
+
 function changeMantra() {
   var randomMantra = Math.floor(Math.random() * mantras.length)
   var receivedMantra = mantras[randomMantra];
   return receivedMantra;
-  selectedMessage.innerText = showMantra;
   showHiddenMessage()
 }
-
 var currentMantra = changeMantra();
-//console.log(currentMantra);
 
 // 👇🏼 Get a random affirmation
 function changeAffirmation() {
   var randomAffirmation = Math.floor(Math.random() * affirmations.length)
   var receivedAffirmation = affirmations[randomAffirmation];
   return receivedAffirmation;
-  selectedMessage.innerText = showAffirmation;
   showHiddenMessage()
 }
-
 var currentAffirmation = changeAffirmation();
-//console.log(currentAffirmation);
 
 const radios = document.querySelectorAll('input')
 for (const radio of radios) {
   radio.onclick = (e) => {
-    console.log(e.target.value);
+    receiveMessage();
+    console.log(chosenMessage);
   }
 }
 
-// function receiveMessage() {
-//   // var radios = document.getElementsByName("message");
-//     for(i = 0; i < radios.length; i++) {
-//       if(radios[0].checked) {
-//         return currentAffirmation;
-//           document.getElementById("#result").innerHTML = currentAffirmation;
-//     } else if (radios[1].checked); {
-//         return currentMantra;
-//           document.getElementById("#result").innerHTML = currentMantra;
-//     }
-//   }
-// }
+function receiveMessage() {
+    for(i = 0; i < radios.length; i++) {
+    } if (radios[0].checked) {
+        return currentAffirmation;
+    } else if (radios[1].checked); {
+        return currentMantra;
+  }
+}
+var chosenMessage = receiveMessage()
 
 // 👇🏼 Show the message when button is pushed
-function receiveMessage() {
-  if (affirmationRadio.checked) {
-    showHiddenMessage();
-    currentAffirmation;
-  } else if (mantraRadio.checked) {
-    currentMantra;
-  } else if (affirmationRadio.checked === false && mantraRadio.checked === false) {
-    window.alert("Please make a selection");
+function pushButton(event) {
+  event.preventDefault();
+  if (!radios[0].checked  && !radios[1].checked) {
+      window.alert("Please make a selection");
+  } else {
+    selectedMessage.classList.remove("hidden");
+    lotusPose.classList.add("hidden");
+    selectedMessage.innerText = chosenMessage;
+    clickToClear.classList.remove("hidden");
   }
 }
 
-function showHiddenMessage() {
-  lotusPose.classList.add('hidden');
-  selectedMessage.classList.remove('hidden');
+function clearAllRadios() {
+  var radList = document.getElementsByName("message");
+  for (var i = 0; i < radList.length; i++) {
+    var radioButton = radList[i];
+    radioButton.checked = false;
+    //if(radList[i].checked) radList[i].checked = false;
+  }
 }
 
-
-
-//
-// clickOnClearButton() {
-//   soundBox.innerHTML = meditatingPerson;
-// }
+function clickToClearButton() {
+  homeView();
+  clearAllRadios();
+}
