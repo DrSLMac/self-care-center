5bce9c4d718e802b77f6273a343194a9e82367fb
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-05-30 01:20:10 -0500

    add function to radios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-//User clicks on one of two radio buttons.
-// The radio button will determine which array is used to randomly pick the message.
-// Once the radio button is clicked, user will click "receive message button".
-//  receive message button click will:
-//  hide meditatingPerson
-//  reveal randomized message.
-//when radio button is clicked, it will create a randomly generated message.
-//clicking the "receive message button" will display the randomly generated message.
-
 // Identify variables
 var affirmationRadio = document.getElementById('radio1');
 var mantraRadio = document.getElementById('radio2');
@@ -14,62 +5,43 @@ var receiveButton = document.querySelector(".receiveMessageButton")
 var receivedMessage = document.querySelector("receive-message")
 var currentMessage ;
 
-// addEventListener('click', function)
-//receiveButton.addEventListener('click', receiveMessageButton);
-// affirmationRadio.addEventListener('onClick', changeAffirmation)
-// mantraRadio.addEventListener('onClick', changeMantra)
-// receiveButton.addEventListener('click', displayRadioValue)
+// 👇🏼 addEventListener('click', function)
+receiveButton.addEventListener('click', receiveMessage)
 
-// Event handlers & functions
 
-// function changeMantra() {
-//   var randomMantra = Math.floor(Math.random() * mantras.length)
-//   var receivedMantra = mantras[randomMantra];
-//   return receivedMantra;
-// }
-
-// function changeAffirmation() {
-//   var randomAffirmation = Math.floor(Math.random() * affirmations.length)
-//   var receivedAffirmation = affirmations[randomAffirmation];
-//   return receivedAffirmation;
-// }
-
+// 👇🏼 Get a random mantra
 function changeMantra() {
-  var showMantra = mantras[Math.floor(Math.random() * mantras.length)];
-  receivedMessage.innerText = showMantra;
+  var randomMantra = Math.floor(Math.random() * mantras.length)
+  var receivedMantra = mantras[randomMantra];
+  return receivedMantra;
+  selectedMessage.innerText = showMantra;
   showHiddenMessage()
 }
 
 var currentMantra = changeMantra();
-console.log(currentMantra);
+//console.log(currentMantra);
 
+// 👇🏼 Get a random affirmation
 function changeAffirmation() {
-  var showAffirmation = affirmations[Math.floor(Math.random() * affirmations.length)];
-  receivedMessage.innerText = showAffirmation;
+  var randomAffirmation = Math.floor(Math.random() * affirmations.length)
+  var receivedAffirmation = affirmations[randomAffirmation];
+  return receivedAffirmation;
+  selectedMessage.innerText = showAffirmation;
   showHiddenMessage()
 }
 
 var currentAffirmation = changeAffirmation();
-console.log(currentAffirmation);
+//console.log(currentAffirmation);
 
-function receiveMessage() {
-  if (affirmationRadio.checked) {
-    changeAffirmation();
-  } else (mantraRadio.checked)
-    changeMantra();
-  // } else if (affirmationRadio.checked = false && mantraRadio.checked = false) {
-  //   window.alert("Please make a selection");
-  // }
+const radios = document.querySelectorAll('input')
+for (const radio of radios) {
+  radio.onclick = (e) => {
+    console.log(e.target.value);
+  }
 }
 
-function showHiddenMessage() {
-  lotusPose.classList.add('hidden');
-  selectedMessage.classList.remove('hidden');
-}
-
-
-// function displayRadioValue() {
-//   var radios = document.getElementsByName("message");
+// function receiveMessage() {
+//   // var radios = document.getElementsByName("message");
 //     for(i = 0; i < radios.length; i++) {
 //       if(radios[0].checked) {
 //         return currentAffirmation;
@@ -80,6 +52,24 @@ function showHiddenMessage() {
 //     }
 //   }
 // }
+
+// 👇🏼 Show the message when button is pushed
+function receiveMessage() {
+  if (affirmationRadio.checked) {
+    showHiddenMessage();
+    currentAffirmation;
+  } else if (mantraRadio.checked) {
+    currentMantra;
+  } else if (affirmationRadio.checked === false && mantraRadio.checked === false) {
+    window.alert("Please make a selection");
+  }
+}
+
+function showHiddenMessage() {
+  lotusPose.classList.add('hidden');
+  selectedMessage.classList.remove('hidden');
+}
+
 
 
 //
